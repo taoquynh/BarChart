15462c54e9f1152f44651b816e7e3d0ea00507d6
taof <taothuyquynh.ks@gmail.com>
2019-05-08 11:33:46 +0700

    bar chart

---
+++ BarChart/BarChart/BarChart/BarEntry.swift
@@ -0,0 +1,23 @@
+//
+//  BarEntry.swift
+//  BarChart
+//
+//  Created by Taof on 5/8/19.
+//  Copyright © 2019 Taof. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct BarEntry {
+    let color: UIColor
+    
+    /// Ranged from 0.0 to 1.0
+    let height: Float
+    
+    /// To be shown on top of the bar
+    let textValue: String
+    
+    /// To be shown at the bottom of the bar
+    let title: String
+}
